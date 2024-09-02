2e895f98cb1871a1e00ce70d136174ec8a3ad871
Nkosi Phila <79625244+Nkosi2000@users.noreply.github.com>
2024-09-02 12:42:42 +0200

    tt

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private navCtrl: NavController) {}
 
+  navigateToScorePage() {
+    this.navCtrl.navigateForward('/score'); // Adjust the route path as needed
+  }
+
+  // navigateToGroupsPage() {
+  //   this.navCtrl.navigateForward('/groups'); // Adjust the route path as needed
+  // }
 }
