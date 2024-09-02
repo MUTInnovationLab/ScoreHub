290c918178c1c21cce2aba26618e32b89c62d14a
Slindokuhl <slindokuhleatlehang22009757@gmail.com>
2024-09-02 14:36:00 +0200

    changes from slikx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire/compat'; // Import AngularFireModule
+import { AngularFireAuthModule } from '@angular/fire/compat/auth'; // Import AngularFireAuthModule
+import { environment } from '../environments/environment'; // Import environment configuration
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase), // Initialize Firebase
+    AngularFireAuthModule // Add AngularFireAuthModule for authentication
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
