125daf1be506d6caabd7c8f080c33da68e64e027
Nkosi Phila <79625244+Nkosi2000@users.noreply.github.com>
2024-09-02 10:47:11 +0200

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyAYpgqCU9WPNPGHDxH5zdHtxlzpdBcmCiQ",
+  authDomain: "markingsystemyear1.firebaseapp.com",
+  projectId: "markingsystemyear1",
+  storageBucket: "markingsystemyear1.appspot.com",
+  messagingSenderId: "744834353834",
+  appId: "1:744834353834:web:be08a5ff0f8545c6ca1ed3",
+  measurementId: "G-BNJH4CQW14"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
